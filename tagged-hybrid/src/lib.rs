60ec12cba975460e6b39f162524ac9a887d7ac9b
Eric Yu <zbrachinara@gmail.com>
2022-08-24 08:47:54 -0400

    redid the proc macro to accept enums

--- tagged-hybrid/src/lib.rs
+++ tagged-hybrid/src/lib.rs
@@ -4,7 +4,8 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Literal, Punct, TokenStream as TokenStream2, TokenTree};
+use proc_macro2::{Literal, TokenStream as TokenStream2, TokenTree};
+use quote::{quote, ToTokens};
 use venial::Declaration;
 
 #[proc_macro_attribute]
@@ -18,17 +19,25 @@ pub fn hybrid_tagged(attr: TokenStream, item: TokenStream) -> TokenStream {
 fn hybrid_tagged_impl(attr: TokenStream2, item: TokenStream2) -> TokenStream {
     let decl = venial::parse_declaration(item);
 
-    let Ok(Declaration::Struct(typed_data)) = decl else {
-        panic!("hybrid_tagged is meant to be invoked on a struct")
+    let Ok(Declaration::Enum(tagged_data)) = decl else {
+        panic!("hybrid_tagged is meant to be invoked on an enum")
     };
 
     let ls = attr_list(attr);
+
+    let ret = {
+        let tag = ls.get("tag").to_token_stream();
+        let str = tagged_data.to_token_stream();
+        quote!(#[serde(tag=#tag)] #str)
+    };
+
     println!("{ls:?}");
+    println!("{ret}");
 
     todo!()
 }
 
-fn attr_list(attr: TokenStream2) -> HashMap<String, Literal> {
+fn attr_list(attr: TokenStream2) -> HashMap<String, TokenTree> {
     attr.into_iter()
         .group_by(|tk| !matches!(tk, TokenTree::Punct(p) if p.as_char() == ','))
         .into_iter()
@@ -36,12 +45,16 @@ fn attr_list(attr: TokenStream2) -> HashMap<String, Literal> {
         .map(|mut triple| {
             let ident = triple.next();
             let eq_sign = triple.next();
-            let lit = triple.next();
+            let value = triple.next();
 
-            assert!(matches!(eq_sign, Some(TokenTree::Punct(eq_sign)) if eq_sign.as_char() == '='));
+            if !matches!(eq_sign, Some(TokenTree::Punct(eq_sign)) if eq_sign.as_char() == '=') {
+                panic!(r#"Attribute arguments should be in the form of `key = value`"#)
+            }
 
-            match (ident, lit) {
-                (Some(TokenTree::Ident(ident)), Some(TokenTree::Literal(lit))) => (ident.to_string(), lit),
+            match (ident, value) {
+                (Some(TokenTree::Ident(ident)), Some(value)) => {
+                    (ident.to_string(), value)
+                }
                 _ => panic!(r#"Attribute arguments should be in the form of `key = "value"`"#),
             }
         })
@@ -56,12 +69,14 @@ mod test {
     #[test]
     fn test_hybrid_tagged_impl() {
         hybrid_tagged_impl(
-            quote!(tag = "type", variant = "varying"),
-            quote!(
-                struct S {
+            quote!(tag = "type", fields = [
                     frame: Number,
                     slack: Swick,
-                    varying: Variations,
+            ]),
+            quote!(
+                #[serde(some_other_thing)]
+                enum Variations {
+                    A, B
                 }
             ),
         );

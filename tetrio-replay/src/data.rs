3d034f036cba1f9ac608f0494dd7a9aa56a50c08
Eric Yu <zbrachinara@gmail.com>
2022-08-23 21:08:41 -0400

    accidentally based the repo on the wrong directory, init commit fr this time

---
+++ tetrio-replay/src/data.rs
@@ -0,0 +1,372 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Number, Value};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TTRM {
+    #[serde(rename = "_id")]
+    pub id: String,
+    pub back: String,
+    pub data: Vec<ReplaySet>,
+    #[serde(rename = "forcestyle")]
+    pub force_style: String,
+    pub gametype: String,
+    #[serde(rename = "ismulti")]
+    pub is_multi: bool,
+    #[serde(rename = "shortid")]
+    pub short_id: String,
+    #[serde(rename = "ts")]
+    pub timestamp: String,
+    pub verified: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ReplaySet {
+    #[serde(rename = "board")]
+    pub boards: Vec<Board>,
+    pub replays: Vec<Replay>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Board {
+    pub active: bool,
+    pub success: bool,
+    pub user: User,
+    pub winning: Number,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    #[serde(rename = "_id")]
+    pub user_id: String,
+    pub username: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Replay {
+    // pub events: Vec<Value>,
+    pub events: Vec<event::Event>,
+    pub frames: Number,
+}
+
+pub mod event {
+    use serde::{Deserialize, Serialize};
+    use serde_json::{Map, Number, Value};
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(from = "EventRaw", into = "EventRaw")]
+    pub struct Event {
+        pub frame: Number,
+        pub data: EventData,
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum EventData {
+        Start,
+        Full(Full),
+        Targets,
+        KeyDown(KeyDown),
+        KeyUp(KeyUp),
+        InGameEvent,
+        End,
+    }
+
+    impl From<EventRaw> for Event {
+        fn from(e: EventRaw) -> Self {
+            match e {
+                EventRaw::Start { frame, .. } => Event {
+                    frame,
+                    data: EventData::Start,
+                },
+                EventRaw::Full { frame, data } => Event {
+                    frame,
+                    data: EventData::Full(data),
+                },
+                EventRaw::Targets { frame } => Event {
+                    frame,
+                    data: EventData::Targets,
+                },
+                EventRaw::KeyDown { frame, data } => Event {
+                    frame: frame,
+                    data: EventData::KeyDown(data),
+                },
+                EventRaw::KeyUp { frame, data } => Event {
+                    frame,
+                    data: EventData::KeyUp(data),
+                },
+                EventRaw::InGameEvent { frame } => Event {
+                    frame,
+                    data: EventData::InGameEvent,
+                },
+                EventRaw::End { frame } => Event {
+                    frame,
+                    data: EventData::End,
+                },
+            }
+        }
+    }
+
+    impl From<Event> for EventRaw {
+        fn from(e: Event) -> Self {
+            let frame = e.frame;
+            match e.data {
+                EventData::Start => EventRaw::Start {
+                    frame,
+                    data: Value::Object(Map::new()),
+                },
+                EventData::Full(data) => EventRaw::Full { frame, data },
+                EventData::Targets => EventRaw::Targets { frame },
+                EventData::KeyDown(data) => EventRaw::KeyDown { frame, data },
+                EventData::KeyUp(data) => EventRaw::KeyUp { frame, data },
+                EventData::InGameEvent => EventRaw::InGameEvent { frame },
+                EventData::End => EventRaw::End { frame },
+            }
+        }
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(tag = "type", rename_all = "lowercase")]
+    enum EventRaw {
+        Start {
+            frame: Number,
+            data: Value,
+        },
+        Full {
+            frame: Number,
+            data: Full,
+        },
+        Targets {
+            frame: Number,
+        },
+        KeyDown {
+            frame: Number,
+            data: KeyDown,
+        },
+        KeyUp {
+            frame: Number,
+            data: KeyUp,
+        },
+        #[serde(rename = "ige")]
+        InGameEvent {
+            frame: Number,
+        },
+        End {
+            frame: Number,
+        },
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(deny_unknown_fields)]
+    pub struct KeyUp {
+        key: String,
+        subframe: Number,
+        hoisted: Option<bool>, //TODO: Figure out what this means
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(deny_unknown_fields)]
+    pub struct KeyDown {
+        key: String,
+        subframe: Number,
+        hoisted: Option<bool>, //TODO: Figure out what this means
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(deny_unknown_fields)]
+    pub struct Full {
+        #[serde(rename = "aggregatestats")]
+        pub aggregate_stats: AggregateStats,
+        pub assumptions: Map<String, Value>,
+        pub fire: Number,
+        pub game: Game,
+        #[serde(rename = "gameoverreason")]
+        pub game_over_reason: Option<String>,
+        pub killer: Killer,
+        pub options: GameOptions,
+        pub replay: Value, //TODO: gonna have to see what these structs mean
+        pub source: Value,
+        pub stats: Value,
+        pub successful: bool,
+        pub targets: Vec<Value>, // TODO: probably string, but have to check
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(deny_unknown_fields)]
+    pub struct GameOptions {
+        #[serde(rename = "allow180")]
+        pub allow_180: bool,
+        pub allow_harddrop: bool,
+        pub are: Number,
+        #[serde(rename = "b2bchaining")]
+        pub b2b_chaining: bool,
+        #[serde(rename = "bagtype")]
+        pub bag_type: String, // change to enum
+        #[serde(rename = "boardbuffer")]
+        pub board_buffer: Number,
+        #[serde(rename = "boardheight")]
+        pub board_height: Number,
+        #[serde(rename = "boardskin")]
+        pub board_skin: String,
+        #[serde(rename = "boardwidth")]
+        pub board_width: Number,
+        pub clutch: bool,
+        pub countdown: bool,
+        /// how many counts before GO (for example, countdown_count = 3 means 3, 2, 1, GO, and game starts immediately on GO)
+        pub countdown_count: Number,
+        /// the amount of time between two counts measured in milliseconds
+        pub countdown_interval: Number,
+        pub display_fire: bool,
+        pub display_hold: bool,
+        pub display_next: bool,
+        pub display_shadow: bool,
+        pub display_username: bool,
+        pub forfeit_time: Number,
+        #[serde(rename = "fullinterval")]
+        pub full_interval: Number,
+        #[serde(rename = "fulloffset")]
+        pub full_offset: Number,
+        #[serde(rename = "g")]
+        pub gravity: Number,
+        #[serde(rename = "garbagecap")]
+        pub garbage_cap: Number,
+        #[serde(rename = "garbagecapincrease")]
+        pub garbage_cap_increase: Number,
+        #[serde(rename = "garbagecapmax")]
+        pub garbage_cap_max: Number,
+        #[serde(rename = "garbageincrease")]
+        pub garbage_increase: Number,
+        #[serde(rename = "garbagemargin")]
+        pub garbage_margin: Number,
+        #[serde(rename = "garbagemultiplier")]
+        pub garbage_multiplier: Number,
+        #[serde(rename = "garbagespeed")]
+        pub garbage_speed: Number,
+        #[serde(rename = "ghostskin")]
+        pub ghost_skin: String,
+        #[serde(rename = "gincrease")]
+        pub gravity_increase: Number,
+        #[serde(rename = "gmargin")]
+        pub gravity_margin: Number,
+        handling: Handling,
+        #[serde(rename = "hasgarbage")]
+        pub has_garbage: bool,
+        #[serde(rename = "infinitemovement")]
+        pub infinite_movement: bool,
+        pub kickset: String,
+        #[serde(rename = "latencypreference")]
+        pub latency_preference: String, //TODO: Probably has to do with passthrough, but should check
+        pub lineclear_are: Number,
+        #[serde(rename = "lockresets")]
+        pub lock_resets: Number,
+        #[serde(rename = "locktime")]
+        pub lock_time: Number,
+        pub manual_allowed: bool,
+        #[serde(rename = "minoskin")]
+        pub tetromino_skin: TetrominoSkin,
+        pub mission: Option<String>,
+        pub mission_type: String,
+        #[serde(rename = "neverstopbgm")]
+        pub loop_bgm: bool,
+        #[serde(rename = "nextcount")]
+        pub next_count: Number,
+        pub objective: Value, // TODO: confirm -- this probably refers to lines cleared to finish in singleplayer games and such
+        #[serde(rename = "onfail")]
+        pub on_fail: Value, // TODO: Find the types of these
+        #[serde(rename = "onfinish")]
+        pub on_finish: Value,
+        #[serde(rename = "oninteraction")]
+        pub on_interaction: Value,
+        pub passthrough: bool,
+        pub physical: bool,
+        #[serde(rename = "precountdown")]
+        pub pre_countdown: Number,
+        #[serde(rename = "prestart")]
+        pub pre_start: Number,
+        pub room_handling: bool,
+        pub room_handling_arr: Number,
+        pub room_handling_das: Number,
+        pub room_handling_sdf: Number,
+        pub seed: Number,
+        pub seed_random: bool,
+        pub slot_bar1: String,
+        pub slot_counter1: Option<String>,
+        pub slot_counter2: Option<String>,
+        pub slot_counter3: Option<String>,
+        pub slot_counter4: Option<String>,
+        pub slot_counter5: Option<String>,
+        #[serde(rename = "spinbonuses")]
+        pub spin_bonuses: String,
+        pub stock: Number,
+        pub username: String,
+        pub version: Number,
+        #[serde(rename = "zoominto")]
+        pub zoom_into: String,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct TetrominoSkin {
+        i: String,
+        j: String,
+        l: String,
+        o: String,
+        s: String,
+        t: String,
+        z: String,
+        other: String,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Killer {
+        pub name: Option<String>,
+        #[serde(rename = "type")]
+        pub kind: String,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct AggregateStats {
+        pub apm: Number,
+        pub pps: Number,
+        pub vsscore: Number,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Game {
+        pub bag: Vec<String>,
+        /// null or string (one of "ljzsoti" or "gb" for garbage)
+        pub board: Vec<Vec<Option<String>>>,
+        pub controlling: Controlling,
+        #[serde(rename = "g")]
+        pub gravity: Number,
+        pub handling: Handling,
+        pub hold: Hold,
+        pub playing: bool,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Hold {
+        pub locked: bool,
+        pub piece: Option<String>,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Handling {
+        pub arr: Number,
+        pub cancel: bool,
+        pub das: Number,
+        pub dcd: Number,
+        pub safelock: bool,
+        pub sdf: Number,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Controlling {
+        #[serde(rename = "lastshift")]
+        pub last_shift: Number,
+        pub ldas: Number,
+        pub ldasiter: Number, //TODO should rename sometime
+        pub lshift: bool,
+        pub rdas: Number,
+        pub rdasiter: Number, //TODO this too
+        pub rshift: bool,
+        pub softdrop: bool,
+    }
+}
---
+++ tetrio-replay/src/lib.rs
@@ -0,0 +1,47 @@
+#![feature(let_else)]
+
+use std::{fs::read_to_string, path::Path};
+
+use data::{TTRM, event::Event};
+
+mod data;
+
+pub fn parse_replay(path: impl AsRef<Path>) {
+    let obj: TTRM = serde_json::from_str(&read_to_string(path).unwrap()).unwrap();
+
+    // println!("{:?}", obj.data[0].replays[0].events[10]);
+
+    // let Value::Object(ref data) = obj.data[0].replays[0] else {panic!()};
+
+    // data.iter().for_each(|(k, v)| {
+    //     match v {
+    //         Value::Array(_) => println!("{k}: array..."),
+    //         Value::Object(_) => println!("{k}: object..."),
+    //         _ => println!("{k}: {v}")
+    //     }
+    // });
+    // obj.data.iter().for_each(|set| {
+    //     set.replays
+    //         .iter()
+    //         .for_each(|replay| replay.events.iter().for_each(
+    //             |event| {
+    //                 match event.data {
+    //                     _ => ()
+    //                 }
+    //             }
+    //         ))
+    // });
+
+    println!("{:#?}", obj.data[0].replays)
+    // println!("{}", obj.data.len())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_replay_test() {
+        parse_replay("src/HBSQabUhSS.ttrm")
+    }
+}

b6df6f822e98755864e582bfe783361e218f8b82
Eric Yu <zbrachinara@gmail.com>
2022-08-25 21:28:13 -0400

    switch to using anyhow for result handling

--- tetrio-replay/src/lib.rs
+++ tetrio-replay/src/lib.rs
@@ -3,31 +3,11 @@
 use std::{fs::read_to_string, path::Path};
 
 pub use data::TTRM;
-use thiserror::Error;
+use anyhow::Result;
 
 pub mod data;
 
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("Serde error: {0}")]
-    Serde(serde_json::Error),
-    #[error("File error: {0}")]
-    Fs(std::io::Error)
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(s: serde_json::Error) -> Self {
-        Self::Serde(s)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(s: std::io::Error) -> Self {
-        Self::Fs(s)
-    }
-}
-
-pub fn parse_replay(path: impl AsRef<Path>) -> Result<TTRM, Error> {
+pub fn parse_replay(path: impl AsRef<Path>) -> Result<TTRM> {
     Ok(serde_json::from_str(&read_to_string(path)?)?)
 }
 

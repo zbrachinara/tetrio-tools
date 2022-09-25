c9f40924188f9a5a35c394fd07b590903bc0e702
Eric Yu <zbrachinara@gmail.com>
2022-09-25 18:11:10 -0400

    make a functional variant of rng array shuffle

--- tetrio-replay/src/rng.rs
+++ tetrio-replay/src/rng.rs
@@ -4,7 +4,7 @@ pub struct Rng {
 
 impl Rng {
     fn seeded(seed: u64) -> Self {
-        Rng { state: seed }
+        Rng { state: seed % 2147483647 }
     }
 
     fn next(&mut self) -> u64 {
@@ -16,15 +16,42 @@ impl Rng {
         (self.next() - 1) as f64 / 2147483646.0
     }
 
-    fn shuffle_array<T>(&mut self, arr: &mut [T]) {
-        (0..arr.len())
+    fn shuffle_slice<T>(&mut self, slice: &mut [T]) {
+        (0..slice.len())
             .rev()
             .map(|i| {
                 let swap_with = (self.next_float() * (i + 1) as f64).floor() as usize;
                 (i, swap_with)
             })
             .for_each(|(a, b)| {
-                arr.swap(a, b);
+                slice.swap(a, b);
             });
+    }
+
+    fn shuffle_array<T, const N: usize>(&mut self, mut arr: [T; N]) -> [T; N] {
+        self.shuffle_slice(&mut arr);
+        arr
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Rng;
+
+    #[test]
+    fn based() {
+        let mut base = ['z', 'l', 'o', 's', 'i', 'j', 't'];
+
+        // game 1 of the file included with this source
+        let mut rng = Rng::seeded(1742887628);
+
+        assert_eq!(
+            rng.shuffle_array(base.clone()),
+            ['z', 'l', 'i', 'o', 'j', 't', 's']
+        );
+        assert_eq!(
+            rng.shuffle_array(base.clone()),
+            ['i', 't', 'j', 'l', 'o', 'z', 's']
+        );
     }
 }

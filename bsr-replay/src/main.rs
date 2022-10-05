2c8d506cb667b617c48a392d785a971665d39b71
Eric Yu <zbrachinara@gmail.com>
2022-10-05 10:55:01 -0400

    boilerplate glium code

--- bsr-replay/src/main.rs
+++ bsr-replay/src/main.rs
@@ -1,3 +1,38 @@
+use glium::{
+    glutin::{
+        event::{Event, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+        ContextBuilder,
+    },
+    Surface,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("Hello world!");
+    let windowed_context = ContextBuilder::new();
+
+    let display = glium::Display::new(wb, windowed_context, &el).unwrap();
+
+    el.run(move |ev, _, control_flow| {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+
+        *control_flow = ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            _ => (),
+        }
+    });
 }

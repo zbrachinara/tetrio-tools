d6fcfe97404798f53ca5afb7905cd32d36f1c1c2
Eric Yu <zbrachinara@gmail.com>
2022-10-05 17:10:33 -0400

    vertex impementation

--- bsr-replay/src/main.rs
+++ bsr-replay/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use glium::{
     glutin::{
         event::{Event, WindowEvent},
@@ -5,8 +7,18 @@ use glium::{
         window::WindowBuilder,
         ContextBuilder,
     },
-    Surface,
+    implement_vertex,
+    index::{NoIndices, PrimitiveType},
+    uniforms::EmptyUniforms,
+    Program, Surface, VertexBuffer,
 };
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
 
 fn main() {
     let el = EventLoop::new();
@@ -15,13 +27,58 @@ fn main() {
 
     let display = glium::Display::new(wb, windowed_context, &el).unwrap();
 
+    let vertex_shader = r#"
+#version 140
+
+in vec2 position;
+out vec4 color_id;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    color_id = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"#;
+
+    let fragment_shader = r#"
+#version 140
+
+in vec4 color_id;
+out vec4 color;
+
+void main() {
+    color = color_id;
+}
+"#;
+
+    let shader = Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = [vertex1, vertex2, vertex3];
+
+    let vbuffer = VertexBuffer::new(&display, &shape).unwrap();
+
     el.run(move |ev, _, control_flow| {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target.draw(
+            &vbuffer,
+            &NoIndices(PrimitiveType::TrianglesList),
+            &shader,
+            &EmptyUniforms,
+            &Default::default(),
+        ).unwrap();
+
         target.finish().unwrap();
 
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time = Instant::now() + Duration::from_nanos(16_666_667);
 
         *control_flow = ControlFlow::WaitUntil(next_frame_time);
         match ev {

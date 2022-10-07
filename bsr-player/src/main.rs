0a7cb5e8969386d5e3210fa74e94d3af05b60ae0
Eric Yu <zbrachinara@gmail.com>
2022-10-07 12:31:08 -0400

    remove unused shaders

--- bsr-player/src/main.rs
+++ bsr-player/src/main.rs
@@ -1,21 +1,22 @@
 use std::time::{Duration, Instant};
 
-use bsr_tools::{board::{Board, Mino, MinoVariant, Direction, Cell}, rng::PieceQueue};
+use bsr_tools::{
+    board::{Board, Cell, Direction, Mino, MinoVariant},
+    rng::PieceQueue,
+};
 use glium::{
     glutin::{
         event::{Event, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
         window::WindowBuilder,
         ContextBuilder,
-    }, Program, Surface,
+    },
+    Surface,
 };
 use gridly_grids::VecGrid;
 
-use crate::board::board_vertex_buffer;
-
-mod draw;
 mod board;
-
+mod draw;
 
 fn main() {
     let el = EventLoop::new();
@@ -24,66 +25,27 @@ fn main() {
 
     let display = glium::Display::new(wb, windowed_context, &el).unwrap();
 
-    let vertex_shader = r#"
-#version 140
-
-in vec2 position;
-in uint color_id;
-flat out uint color_id_out;
-
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-    color_id_out = color_id;
-}
-"#;
-
-    let fragment_shader = r#"
-#version 140
-
-flat in uint color_id_out;
-out vec4 color;
-
-void main() {
-    switch (color_id_out) {
-        case 0u: // L piece
-            color = vec4(1.0, 0.1, 0.0, 1.0);
-            break;
-        case 1u: // J piece
-            color = vec4(0.0, 0.0, 1.0, 1.0);
-            break;
-        case 2u: // T piece
-            color = vec4(0.5, 0.0, 1.0, 1.0);
-            break;
-        case 3u: // Z piece
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-            break;
-        case 4u: // S piece
-            color = vec4(0.1, 1.0, 0.0, 1.0);
-            break;
-        case 5u: // O piece
-            color = vec4(1.0, 1.0, 0.0, 1.0);
-            break;
-        case 6u: // I piece
-            color = vec4(0.0, 1.0, 1.0, 1.0);
-            break;
-    }
-}
-"#;
-
-    let shader = Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
     let draw_grid = draw::grid::DrawProgram::new(&display, (10, 20));
     let draw_board = board::DrawBoard::new(&display);
 
-
-
-    use MinoVariant::*;
-    use Cell::Tetromino as Tet;
     use Cell::Garbage as Gb;
     use Cell::None as Nn;
+    use Cell::Tetromino as Tet;
+    use MinoVariant::*;
     let example_board = Board {
-        cells: VecGrid::new_from_rows([
-            [Tet(J), Tet(J), Nn, Tet(I), Tet(I), Tet(I), Tet(I), Tet(S), Tet(O), Tet(O)]
-        ]).unwrap(),
+        cells: VecGrid::new_from_rows([[
+            Tet(J),
+            Tet(J),
+            Nn,
+            Tet(I),
+            Tet(I),
+            Tet(I),
+            Tet(I),
+            Tet(S),
+            Tet(O),
+            Tet(O),
+        ]])
+        .unwrap(),
         queue: PieceQueue::meaningless(),
         active: Mino {
             variant: MinoVariant::T,
@@ -97,7 +59,9 @@ void main() {
         target.clear_color(0.0, 0.0, 0.0, 1.0);
 
         draw_grid.draw_grid(&mut target).unwrap();
-        draw_board.draw_board(&display, &mut target, &example_board).unwrap();
+        draw_board
+            .draw_board(&display, &mut target, &example_board)
+            .unwrap();
         // target.draw(
         //     &vbuffer,
         //     &NoIndices(PrimitiveType::TrianglesList),

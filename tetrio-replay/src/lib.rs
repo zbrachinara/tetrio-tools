fc9de42a7b29a75300bce0345277c5b47126d107
zbrachinara <zbrachinara@gmail.com>
2023-03-05 15:04:55 -0500

    remove items related to ttrm parsing

--- tetrio-replay/src/lib.rs
+++ tetrio-replay/src/lib.rs
@@ -1,26 +1,5 @@
 #![feature(drain_filter)]
 
-use anyhow::Result;
-use ttrm::Ttrm;
-
 mod board;
 mod reconstruct;
 mod rng;
-
-pub fn parse_replay(content: &str) -> Result<Ttrm> {
-    Ok(serde_json::from_str(content)?)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs::read_to_string;
-
-    #[test]
-    fn parse_replay_test() {
-        println!(
-            "{:#?}",
-            parse_replay(&read_to_string("src/HBSQabUhSS.ttrm").unwrap()).unwrap()
-        );
-    }
-}

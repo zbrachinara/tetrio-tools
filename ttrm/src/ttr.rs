548604a8fb95f223e76059c09fb1483828ca0bfa
zbrachinara <zbrachinara@gmail.com>
2023-03-04 21:28:04 -0500

    endcontext struct in ttr

--- ttrm/src/ttr.rs
+++ ttrm/src/ttr.rs
@@ -1,10 +1,87 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use serde_json::Number;
 
 use crate::User;
 
-
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Ttr<'a> {
     #[serde(borrow)]
-    pub user: User<'a>
+    pub user: User<'a>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EndContext {
+    pub seed: u64,
+    pub lines: u64,
+    pub level_lines_needed: u64,
+    pub inputs: u64,
+    pub holds: u64,
+    pub time: Time,
+    pub score: u64,
+    #[serde(rename = "zenlevel")]
+    pub zen_level: u64,
+    #[serde(rename = "zenprogress")]
+    pub zen_progress: u64,
+    pub level: u64,
+    pub combo: u64,
+    #[serde(rename = "currentcombopower")]
+    pub combo_power: u64,
+    #[serde(rename = "topcombo")]
+    pub top_combo: u64,
+    pub btb: u64,
+    #[serde(rename = "topbtb")]
+    pub top_b2b: u64,
+    #[serde(rename = "currentbtbchainpower")]
+    pub current_b2b_power: u64,
+    pub tspins: u64,
+    #[serde(rename = "piecesplaced")]
+    pub pieces_placed: u64,
+    pub clears: Clears,
+    pub garbage: Garbage,
+    pub kills: u64,
+    pub finesse: Finesse,
+    #[serde(rename = "finalTime")]
+    pub final_time: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Time {
+    pub start: f64,
+    pub zero: bool,
+    pub locked: bool,
+    pub prev: u64,
+    #[serde(rename = "frameoffset")]
+    pub frame_offset: Number, // could possibly be u8
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Clears {
+    pub singles: u64,
+    pub doubles: u64,
+    pub triples: u64,
+    pub quads: u64,
+    pub realtspins: u64,
+    pub minitspins: u64,
+    pub minitspinsingles: u64,
+    pub tspinsingles: u64,
+    pub minitspindoubles: u64,
+    pub tspindoubles: u64,
+    pub tspintriples: u64,
+    pub tspinquads: u64,
+    pub allclear: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Garbage {
+    pub sent: u64,
+    pub received: u64,
+    pub attack: u64,
+    pub cleared: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Finesse {
+    pub combo: u64,
+    pub faults: u64,
+    pub perfectpieces: u64,
 }

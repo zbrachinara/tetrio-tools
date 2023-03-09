0511632fd8c25a25d36c8cf12b0af1769d5d0659
zbrachinara <zbrachinara@gmail.com>
2023-03-08 20:29:19 -0500

    extract common testing code to function

--- tetrio-replay/tests/ttr.rs
+++ tetrio-replay/tests/ttr.rs
@@ -1,28 +1,33 @@
-use std::fs::OpenOptions;
-use std::io::Write;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write};
+use std::path::Path;
 
+use bsr_tools::action::Action;
 use tetrio_replay::reconstruct;
 
-#[test]
-fn reconstruct_40l() {
-    let ttr = serde_json::from_slice::<ttrm::Ttr>(include_bytes!("samples/40l.ttr"))
+fn reconstruct_from_bytes(bytes: &[u8], write_to: &str) -> Result<(), Vec<Action>> {
+    let ttr = serde_json::from_slice::<ttrm::Ttr>(bytes)
         .expect("TTR parsing is not working correctly, check tests in ttr crate");
 
     let action_list = reconstruct(&ttr.data.events).expect("Reconstruction step failed");
 
-    if OpenOptions::new()
+    OpenOptions::new()
         .create(true)
         .truncate(true)
         .write(true)
-        .open("40l.out")
+        .open(write_to)
         .and_then(|mut out_file| {
             action_list
                 .clone()
                 .into_iter()
                 .try_for_each(|action| writeln!(out_file, "{action:?}"))
         })
-        .is_err()
-    {
+        .map_err(|_| action_list)
+}
+
+#[test]
+fn reconstruct_40l() {
+    if let Err(action_list) = reconstruct_from_bytes(include_bytes!("samples/40l.ttr"), "40l.out") {
         println!(
             "Test 40l could not open the output file was writing, output going to stderr instead"
         );

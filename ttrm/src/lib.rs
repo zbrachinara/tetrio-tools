f413351b80f7ee2bf77da4fb744caae1d3ce89c9
zbrachinara <zbrachinara@gmail.com>
2023-03-14 09:12:45 -0400

    reading data from files

--- ttrm/src/lib.rs
+++ ttrm/src/lib.rs
@@ -2,5 +2,14 @@ pub mod event;
 mod ttr;
 mod ttrm;
 
+pub use serde_json::Error;
 pub use ttr::*;
 pub use ttrm::*;
+
+pub fn ttr_from_slice(slice: &[u8]) -> Result<Ttr, serde_json::Error> {
+    serde_json::from_slice(slice)
+}
+
+pub fn ttrm_from_slice(slice: &[u8]) -> Result<Ttrm, serde_json::Error> {
+    serde_json::from_slice(slice)
+}
--- viewtris-player/src/main.rs
+++ viewtris-player/src/main.rs
@@ -1,14 +1,45 @@
-use std::fs;
+use std::{fs, os::unix::prelude::OsStrExt};
 
 use draw::board::Board;
 
 use macroquad::prelude::*;
+use tetrio_replay::viewtris::action::Action;
 
 mod draw;
+
+fn open_file() -> Result<Vec<Action>, ()> {
+    rfd::FileDialog::new()
+        .pick_file()
+        .and_then(|fi| {
+            fs::read(fi.clone())
+                .map(|buf| (buf, fi.extension().map(|str| str.to_os_string())))
+                .ok()
+        })
+        .and_then(|(buf, extension)| match extension {
+            Some(x) if x.as_bytes() == b"ttr" => {
+                tetrio_replay::ttrm::ttr_from_slice(buf.as_slice())
+                    .ok()
+                    .and_then(|ttr| tetrio_replay::reconstruct(ttr.data.events.as_slice()).ok())
+            }
+            Some(x) if x.as_bytes() == b"ttrm" => {
+                tetrio_replay::ttrm::ttrm_from_slice(buf.as_slice())
+                    .ok()
+                    .and_then(|ttrm| {
+                        tetrio_replay::reconstruct(ttrm.data[0].replays[0].events.as_slice()).ok()
+                    })
+            }
+            _ => {
+                eprintln!("Unknown file type, this player only expects ttr or ttrm ");
+                None
+            }
+        })
+        .ok_or(())
+}
 
 #[macroquad::main("bsr player")]
 async fn main() {
     let mut board = Board::empty();
+    let mut actions = Vec::<Action>::new();
 
     loop {
         clear_background(BLACK);
@@ -16,10 +47,9 @@ async fn main() {
         if is_key_pressed(KeyCode::O)
             && (is_key_down(KeyCode::LeftControl) || is_key_down(KeyCode::RightControl))
         {
-            if let Some(fi) = rfd::FileDialog::new().pick_file() {
-                board = Board::empty();
-                let buf = fs::read_to_string(fi);
-                println!("{buf:?}")
+            board = Board::empty();
+            if let Ok(new_actions) = open_file() {
+                actions = new_actions;
             }
         }
 

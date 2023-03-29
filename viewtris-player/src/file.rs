cea77f81042493cbf01d3764cea3a6782e2366f1
zbrachinara <zbrachinara@gmail.com>
2023-03-29 08:17:38 -0400

    move file operations into own module

---
+++ viewtris-player/src/file.rs
@@ -0,0 +1,62 @@
+use std::{fs, os::unix::prelude::OsStrExt};
+
+use itertools::Itertools;
+
+use crate::{selection::Selection, state::ReplayState};
+
+pub fn open_file() -> Result<Selection, ()> {
+    rfd::FileDialog::new()
+        .pick_file()
+        .and_then(|fi| {
+            fs::read(fi.clone())
+                .map(|buf| (buf, fi.extension().map(|str| str.to_os_string())))
+                .ok()
+        })
+        .and_then(|(buf, extension)| match extension {
+            Some(x) if x.as_bytes() == b"ttr" => read_ttr(buf.as_slice()),
+            Some(x) if x.as_bytes() == b"ttrm" => read_ttrm(buf.as_slice()),
+            _ => {
+                eprintln!("Unknown file type, this player only expects ttr or ttrm ");
+                None
+            }
+        })
+        .ok_or(())
+}
+
+fn read_ttr(buf: &[u8]) -> Option<Selection> {
+    tetrio_replay::ttrm::ttr_from_slice(buf)
+        .ok()
+        .and_then(|ttr| tetrio_replay::reconstruct(ttr.game_type, ttr.data.events.as_slice()).ok())
+        .map(|actions| Selection {
+            replays: vec![ReplayState::with_actions([actions])],
+            selected: 0,
+            in_replay: true,
+        })
+}
+
+fn read_ttrm(buf: &[u8]) -> Option<Selection> {
+    tetrio_replay::ttrm::ttrm_from_slice(buf)
+        .ok()
+        .and_then(|ttrm| {
+            let replays = ttrm
+                .data
+                .iter()
+                .filter_map(|player| {
+                    player
+                        .replays
+                        .iter()
+                        .map(|replay| {
+                            tetrio_replay::reconstruct(ttrm.game_type, &replay.events).ok()
+                        })
+                        .collect::<Option<Vec<_>>>()
+                        .map(ReplayState::with_actions)
+                })
+                .collect_vec();
+
+            (!replays.is_empty()).then_some(Selection {
+                replays,
+                selected: 0,
+                in_replay: false,
+            })
+        })
+}
--- viewtris-player/src/main.rs
+++ viewtris-player/src/main.rs
@@ -1,71 +1,12 @@
 #![feature(iterator_try_collect)]
 
-use std::{fs, os::unix::prelude::OsStrExt};
-
-use itertools::Itertools;
 use macroquad::prelude::*;
 use selection::Selection;
-use state::ReplayState;
 
 mod draw;
+mod file;
 mod selection;
 mod state;
-
-fn open_file() -> Result<Selection, ()> {
-    rfd::FileDialog::new()
-        .pick_file()
-        .and_then(|fi| {
-            fs::read(fi.clone())
-                .map(|buf| (buf, fi.extension().map(|str| str.to_os_string())))
-                .ok()
-        })
-        .and_then(|(buf, extension)| match extension {
-            Some(x) if x.as_bytes() == b"ttr" => {
-                tetrio_replay::ttrm::ttr_from_slice(buf.as_slice())
-                    .ok()
-                    .and_then(|ttr| {
-                        tetrio_replay::reconstruct(ttr.game_type, ttr.data.events.as_slice()).ok()
-                    })
-                    .map(|actions| Selection {
-                        replays: vec![ReplayState::with_actions([actions])],
-                        selected: 0,
-                        in_replay: true,
-                    })
-            }
-            Some(x) if x.as_bytes() == b"ttrm" => {
-                tetrio_replay::ttrm::ttrm_from_slice(buf.as_slice())
-                    .ok()
-                    .and_then(|ttrm| {
-                        let replays = ttrm
-                            .data
-                            .iter()
-                            .filter_map(|player| {
-                                player
-                                    .replays
-                                    .iter()
-                                    .map(|replay| {
-                                        tetrio_replay::reconstruct(ttrm.game_type, &replay.events)
-                                            .ok()
-                                    })
-                                    .collect::<Option<Vec<_>>>()
-                                    .map(ReplayState::with_actions)
-                            })
-                            .collect_vec();
-
-                        (!replays.is_empty()).then_some(Selection {
-                            replays,
-                            selected: 0,
-                            in_replay: false,
-                        })
-                    })
-            }
-            _ => {
-                eprintln!("Unknown file type, this player only expects ttr or ttrm ");
-                None
-            }
-        })
-        .ok_or(())
-}
 
 #[macroquad::main("Viewtris")]
 async fn main() {
@@ -77,7 +18,7 @@ async fn main() {
         if is_key_pressed(KeyCode::O)
             && (is_key_down(KeyCode::LeftControl) || is_key_down(KeyCode::RightControl))
         {
-            if let Ok(new_actions) = open_file() {
+            if let Ok(new_actions) = file::open_file() {
                 menu = new_actions;
             }
         }
